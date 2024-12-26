d23bd29d87a821a502ddcaaa0e521c9064ced1a4
Kesari Karna <kesaridevaraya@gmail.com>
2024-12-26 22:12:46 +0530

    update2

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -40,7 +40,7 @@ const Hero = () => {
         />
       </div>
 
-      <div className="flex justify-center relative my-20 z-10">
+      <div className="flex justify-center relative mt-40 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Welcome to my portfolio
@@ -52,7 +52,7 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Transforming Concepts into Seamless User Experience"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
